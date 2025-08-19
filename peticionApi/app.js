4b968c79430fe6f3c3bdc5e7658721130a2d43ce
franromano31 <fran.roma1@gmail.com>
2025-08-18 21:43:10 -0300

    Agregamos la peticion a la api

--- peticionApi/app.js
+++ peticionApi/app.js
@@ -4,12 +4,16 @@ const tbody = document.querySelector('#tabla tbody');
 btn.addEventListener('click', async () => {
   tbody.innerHTML = ''; // limpia
 
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await res.json();
-  
-  users.forEach(u => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${u.id}</td><td>${u.name}</td><td>${u.email}</td>`;
-    tbody.appendChild(tr);
-  });
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await res.json();
+
+    users.forEach(u => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `<td>${u.id}</td><td>${u.name}</td><td>${u.email}</td>`;
+      tbody.appendChild(tr);
+    });
+  } catch (e) {
+    console.error('Error al cargar los usuarios', e);
+  }
 });
