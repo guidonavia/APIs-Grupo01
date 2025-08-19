f6f1865ac015649e6826a46d51ae2f9ffb43d8cf
franromano31 <fran.roma1@gmail.com>
2025-08-18 21:15:51 -0300

    improved app

--- peticionApi/app.js
+++ peticionApi/app.js
@@ -3,8 +3,10 @@ const tbody = document.querySelector('#tabla tbody');
 
 btn.addEventListener('click', async () => {
   tbody.innerHTML = ''; // limpia
+
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await res.json();
+  
   users.forEach(u => {
     const tr = document.createElement('tr');
     tr.innerHTML = `<td>${u.id}</td><td>${u.name}</td><td>${u.email}</td>`;
