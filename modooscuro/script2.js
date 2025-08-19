567544c151dc1443de80371d34194bd49b31a957
franromano31 <fran.roma1@gmail.com>
2025-08-18 21:00:25 -0300

    chore: first commit

---
+++ modooscuro/script2.js
@@ -0,0 +1,7 @@
+// document.querySelector('#btn-modo-oscuro').addEventListener('click', function() {
+//     document.querySelector('body').classList.toggle('oscuro');
+// });
+
+function toggleModoOscuro() {
+    document.querySelector('body').classList.toggle('oscuro');
+}
---
+++ peticionApi/app.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('cargar');
+const tbody = document.querySelector('#tabla tbody');
+
+btn.addEventListener('click', async () => {
+  tbody.innerHTML = ''; // limpia
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+  users.forEach(u => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${u.id}</td><td>${u.name}</td><td>${u.email}</td>`;
+    tbody.appendChild(tr);
+  });
+});
