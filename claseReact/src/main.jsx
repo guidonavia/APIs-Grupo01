042795ee4db50e746b67842befe0777eb8278263
guidonavia <guido.n.navia@gmail.com>
2025-08-25 22:09:44 -0300

    Add react web

--- claseReact/src/main.jsx
+++ claseReact/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './App.css'
+import Card from './Card.jsx'
 
 const root = createRoot(document.getElementById('root'))
+
+function onButtonClick(){
+  //Card.style.backgroundColor = 'red';
+  body.backgroundColor = 'red';
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Card
+      title={'Titulo de la pagina'} 
+      description={'Esta pagina esta hecha con React'}
+      onButtonClick={() => onButtonClick}
+      children ={<p>Texto como children</p>}
+      />
   </React.StrictMode>
 )
