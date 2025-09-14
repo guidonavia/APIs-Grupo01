54348aa6a8405f26ed169cb9f2250675ca6eee60
lucas-abalos <lucas22abalos@gmail.com>
2025-09-14 20:38:08 -0300

    Modificaciones para incluir PROTECTED_PATHS y que no pida credenciales al ingresar a la aplicación

--- EcommerceAppv2/frontend/src/App.jsx
+++ EcommerceAppv2/frontend/src/App.jsx
@@ -1,15 +1,2 @@
-import { useAuth } from './context/AuthContext';
-import Login from './components/Login';
 import Home from './layout/Home';
-
-function App() {
-  const { user } = useAuth();
-
-  return (
-    <div className="App">
-      {!user ? <Login /> : <Home />}
-    </div>
-  );
-}
-
-export default App;
+export default function App() { return <Home />; }
